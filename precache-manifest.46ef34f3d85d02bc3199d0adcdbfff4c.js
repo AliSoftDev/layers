2d47111204788327ea27d6af5d0fd353ebc7b0a8
alisena <alisenamudaber@gmail.com>
2019-04-05 21:27:05 +0100

    Updates

---
+++ precache-manifest.46ef34f3d85d02bc3199d0adcdbfff4c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "2fcfc9b8db036657d4e5",
+    "url": "/layers/static/css/main.25883e12.chunk.css"
+  },
+  {
+    "revision": "2fcfc9b8db036657d4e5",
+    "url": "/layers/static/js/main.7ecfd4d7.chunk.js"
+  },
+  {
+    "revision": "930492c7abd86bbde7b9",
+    "url": "/layers/static/js/runtime~main.09fb4d38.js"
+  },
+  {
+    "revision": "adb1af0a211f370186cf",
+    "url": "/layers/static/js/2.c957e8c6.chunk.js"
+  },
+  {
+    "revision": "63ade3f5be1747af6e4b7fb77c89c132",
+    "url": "/layers/static/media/crepes.63ade3f5.jpg"
+  },
+  {
+    "revision": "e417d72264c6b26cc954a99b337a37c7",
+    "url": "/layers/static/media/chocolate_waffle.e417d722.jpg"
+  },
+  {
+    "revision": "33e266e4cc78444c933e4b78fb2307f1",
+    "url": "/layers/static/media/small_cake.33e266e4.jpg"
+  },
+  {
+    "revision": "c62322b25b29336bcd245e8c60c930a6",
+    "url": "/layers/static/media/strawberry.c62322b2.jpg"
+  },
+  {
+    "revision": "2e01bee7c07182335eb1f46ba8ea9110",
+    "url": "/layers/static/media/cereal_bowl.2e01bee7.jpg"
+  },
+  {
+    "revision": "9a42137887c3f417ee1c6eaed1f30368",
+    "url": "/layers/static/media/chocolate_cake.9a421378.jpg"
+  },
+  {
+    "revision": "23d837b2fbfb0f6a0b1175de1e41814c",
+    "url": "/layers/static/media/black_coffee.23d837b2.jpg"
+  },
+  {
+    "revision": "d6a23e50f8e5aa1720ca34b065be8d6f",
+    "url": "/layers/static/media/berries_waffle.d6a23e50.jpg"
+  },
+  {
+    "revision": "5750ad264c24fb89e8af48d95829ed38",
+    "url": "/layers/static/media/smoothie.5750ad26.jpg"
+  },
+  {
+    "revision": "b6dfc38a13694e72d5b33bde013a727e",
+    "url": "/layers/index.html"
+  }
+];
